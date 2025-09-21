8007d56cff1282bea86ca0ecc31e34c94b129c43
ZainUlAbideen02 <241475@students.au.edu.pk>
2025-09-21 21:22:43 +0500

    Add Two Numbers - Linked List - LeetCode - 02

--- main.cpp
+++ main.cpp
@@ -1,170 +1,59 @@
-#include<iostream>
-#include<queue>
-#include<stack>
-#include<algorithm>
-using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode * temp1 = l1;
+        ListNode * temp2 = l2;
+        ListNode * head = NULL;
+        ListNode * temp3;
+        int carry = 0;
 
+        while (temp1 || temp2) {
+            int x = (temp1 ? temp1->val : 0);
+            int y = (temp2 ? temp2->val : 0);
+            int sum = x + y + carry;
 
-class Node{
-   public:
-   int data;
-   Node * left ;
-   Node * right;
+            if (sum < 10) {
+                carry = 0;
+                ListNode * temp = new ListNode(sum);
+                if (head == NULL) {
+                    head = temp;
+                    temp3 = temp;
+                } else {
+                    temp3->next = temp;
+                    temp3 = temp;
+                }
+            } else {
+                int remainder = sum % 10;
+                ListNode * temp = new ListNode(remainder);
+                carry = 1;
+                if (head == NULL) {
+                    head = temp;
+                    temp3 = temp;
+                } else {
+                    temp3->next = temp;
+                    temp3 = temp;
+                }
+            }
 
-   Node(int value){
-    data = value;
-    left = NULL;
-    right = NULL;
-   }
+            temp1 = (temp1 ? temp1->next : nullptr);
+            temp2 = (temp2 ? temp2->next : nullptr);
+        }
+
+        if (carry > 0) {
+            ListNode * temp = new ListNode(carry);
+            temp3->next = temp;
+        }
+
+        return head;
+    }
 };
-
-
-void BinaryTree(Node * root){
-
-    int x = 0; 
-    cin>>x;
-
-    if(x!=-1){
-      root->left = new Node(x);
-      BinaryTree(root->left);
-    }
-    else{
-        root->left = NULL;
-        return ;
-    }
-    cin>>x;
-    if(x!=-1){
-      root->right = new Node(x);
-      BinaryTree(root->right);
-    }
-    else{
-        root->right=NULL;
-        return;
-    }
-    
-}
-
-void preOrderTraversal(Node * root){
-    if(root==NULL){
-        return ;
-    }
-    cout<<root->data<<" ";
-
-    preOrderTraversal(root->left);
-    preOrderTraversal(root->right);
-}
-
-void inOrderTraversal(Node * root){
-    if(root==NULL){
-        return ;
-    }
-    inOrderTraversal(root->left);
-    cout<<root->data<<" ";
-    inOrderTraversal(root->right);
-}
-
-void postOrderTraversal(Node * root){
-    if(root==NULL){
-        return ;
-    }
-    postOrderTraversal(root->left);
-    postOrderTraversal(root->right);
-    cout<<root->data<<" ";
-
-
-}
-
-void leftViewOfBinaryTree(Node * root){
-    queue<Node *>q1;
-    q1.push(root);
-    while(!q1.empty()){
-        int n = q1.size();
-        for(int i = 0 ; i<n;i++){
-            Node * temp = q1.front();
-            q1.pop();
-            if(i==0){
-                cout<<temp->data<<" ";
-            }
-            if(temp->left){
-                q1.push(temp->left);
-            }
-            if(temp->right){
-                q1.push(temp->right);
-            }
-        }
-    }
-}
-
-void rightViewOfBinaryTree(Node * root){
-    queue<Node *>q1;
-    q1.push(root);
-    while(!q1.empty()){
-        int n = q1.size();
-        for(int i = 0 ; i<n;i++){
-            Node * temp = q1.front();
-            q1.pop();
-            if(i==n-1){
-                cout<<temp->data<<" ";
-            }
-            if(temp->left){
-                q1.push(temp->left);
-            }
-            if(temp->right){
-                q1.push(temp->right);
-            }
-        }
-    }
-}
-
-void preIterative(Node * root){
-    stack<Node * >s1;
-    s1.push(root);
-
-    while(!s1.empty()){
-        Node * temp = s1.top();
-        s1.pop();
-        cout<<temp->data<<" ";
-        if(temp->right){
-            s1.push(temp->right);
-        }
-        if(temp->left){
-            s1.push(temp->left);
-        }
-    }
-}
-
-void postIterative(Node * root){
-    stack<Node * >s1;
-    s1.push(root);
-    vector<int>v1;
-
-    while(!s1.empty()){
-        Node * temp = s1.top();
-        s1.pop();
-        v1.push_back(temp->data);
-        if(temp->left){
-            s1.push(temp->left);
-        }
-        if(temp->right){
-            s1.push(temp->right);
-        }
-    }
-    reverse(v1.begin(),v1.end());
-
-    for(int i:v1){
-        cout<<i<<" ";
-    }
-}
-
-int main(){
-    int val;
-    cin >> val;
-    Node* root = new Node(val);
-    BinaryTree(root);
-    cout<<endl;
-    preOrderTraversal(root);
-    cout<<endl;
-    inOrderTraversal(root);
-    cout<<endl;
-    postOrderTraversal(root);
-}
