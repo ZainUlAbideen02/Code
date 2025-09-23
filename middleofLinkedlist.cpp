875c09dffba72bc7d218e4216bb0bab99bcb0603
ZainUlAbideen02 <241475@students.au.edu.pk>
2025-09-23 22:45:29 +0500

    Leetcode 876 - LinkedList - DSA

---
+++ middleofLinkedlist.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode * temp = head;
+        int count = 0;
+        while(temp){
+            count++;
+            temp = temp->next;
+        }
+        int len = (count/2);
+        ListNode * temp2 = head; 
+        while(len >0){
+            temp2 = temp2->next;
+            len--;
+        }
+        return temp2;
+
+    }
+};
